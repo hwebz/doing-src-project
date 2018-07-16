0cd74f5c978c08f775b7af0ef68581a526b9b2c9
Ha Manh Do <hadm.haui@gmail.com>
2018-07-17 00:45:09 +0700

    add addons page

--- app/js/pages/addons.js
+++ app/js/pages/addons.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
+    var navTabsClass = '.navigation-tab';
+    var navContentsClass = '.navigation-content';
+    var navTabs = $(navTabsClass);
+    var navContents = $(navContentsClass);
 
+    if (isDefined(navTabs) && isDefined(navContents)) {
+        navTabs.on('click', function(e) {
+            e.preventDefault();
+            var $this = $(this);
+            var targetClass = $this.attr('data-target');
+            var target = $(targetClass);
+
+            if(isDefined(target)) {
+                if (target.hasClass('active')) {
+                    target.removeClass('active');
+                    $this.removeClass('active');
+                } else {
+                    navContents.removeClass('active');
+                    navTabs.removeClass('active');
+
+                    target.addClass('active');
+                    $this.addClass('active');
+                }
+            }
+
+            return false;
+        });
+    }
 });
